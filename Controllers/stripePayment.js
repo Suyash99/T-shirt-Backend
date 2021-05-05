680b07de4186903b8189f0e579901e6c3b619241
Suyash Kumar <suyashkumar009@gmail.com>
2021-05-05 21:39:06 +0530

    Added skey to bkend

--- Controllers/stripePayment.js
+++ Controllers/stripePayment.js
@@ -1,4 +1,4 @@
-const stripe = require("stripe")();
+const stripe = require("stripe")(process.env.S_KEY);
 const uuid = require("uuid/v4");
 
 exports.makePayment = (req, res) => {
@@ -20,12 +20,27 @@ exports.makePayment = (req, res) => {
     })
     .then((customer) => {
       stripe.charges
-        .create({
-            amount: amount,
-            currency: 'usd',
+        .create(
+          {
+            amount: amount * 100,
+            currency: "usd",
             customer: customer.id,
-            receipt_email: token.email
-        }, { idempotencyKey })
+            receipt_email: token.email,
+            description: "Testing",
+
+            shipping: {
+              name: token.card.name,
+              address: {
+                line1: token.card.address_line1,
+                line2: token.card.address_line2,
+                city: token.card.address_city,
+                country: token.card.address_country,
+                postal_code: token.card.address_zip,
+              },
+            },
+          },
+          { idempotencyKey }
+        )
         .then((result) => res.status(200).json(result))
         .catch((err) => console.log(err));
     });
